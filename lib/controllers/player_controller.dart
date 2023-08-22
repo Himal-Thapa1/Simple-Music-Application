a819574df34a65f02e883e722db9441f4ae39a07
Himal-Thapa1 <himalthapa2020@outlook.com>
2023-08-22 13:48:43 +0545

    I dunno but music is playing for now

--- lib/controllers/player_controller.dart
+++ lib/controllers/player_controller.dart
@@ -1,23 +1,41 @@
 import 'package:get/get.dart';
+import 'package:just_audio/just_audio.dart';
 import 'package:on_audio_query/on_audio_query.dart';
 import 'package:permission_handler/permission_handler.dart';
 
-class PlayerController extends GetxController{
-
+class PlayerController extends GetxController {
   final audioQuery = OnAudioQuery();
+  final audioPlayer = AudioPlayer();
 
   @override
-  void onInit(){
+  void onInit() async {
     super.onInit();
+    await audioPlayer.setAsset('assets/empty.mp3');
     checkPermission();
-
   }
+
+  @override
+  void onClose() {
+    audioPlayer.dispose();
+    super.onClose();
+  }
+
   checkPermission() async {
     var perm = await Permission.storage.request();
-    if(perm.isGranted) {
-    }
-    else{
+    if (perm.isGranted) {
+    } else {
       checkPermission();
+    }
+  }
+
+  playSong(String? uri) {
+    try {
+      audioPlayer.setAudioSource(
+        AudioSource.uri(Uri.parse(uri!)),
+      );
+      audioPlayer.play();
+    } on Exception catch (e) {
+      print(e.toString());
     }
   }
 }
